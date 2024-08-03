cdfdd7a51c6ec2f9ecb9f3a14ca77aede052748b
Marcott <kasangandjomarcott47@gmail.com>
2024-08-03 12:35:05 +0200

    register

--- register.php
+++ register.php
@@ -26,10 +26,10 @@ if(isset($_POST['submit'])){
    $select_users->execute([$email]);
 
    if($select_users->rowCount() > 0){
-      $warning_msg[] = 'email already taken!';
+      $warning_msg[] = 'cet email est deja pris !';
    }else{
       if($pass != $c_pass){
-         $warning_msg[] = 'Password not matched!';
+         $warning_msg[] = 'Le mot de passe ne correspond pas!';
       }else{
          $insert_user = $conn->prepare("INSERT INTO `users`(id, name, number, email, password) VALUES(?,?,?,?,?)");
          $insert_user->execute([$id, $name, $number, $email, $c_pass]);
@@ -56,59 +56,66 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Register</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php include 'components/user_header.php'; ?>
 
-<!-- register section starts  -->
+    <?php include 'components/user_header.php'; ?>
 
-<section class="form-container">
+    <!-- register section starts  -->
 
-   <form action="" method="post">
-      <h3>create an account!</h3>
-      <input type="tel" name="name" required maxlength="50" placeholder="enter your name" class="box">
-      <input type="email" name="email" required maxlength="50" placeholder="enter your email" class="box">
-      <input type="number" name="number" required min="0" max="9999999999" maxlength="10" placeholder="enter your number" class="box">
-      <input type="password" name="pass" required maxlength="20" placeholder="enter your password" class="box">
-      <input type="password" name="c_pass" required maxlength="20" placeholder="confirm your password" class="box">
-      <p>already have an account? <a href="login.html">login now</a></p>
-      <input type="submit" value="register now" name="submit" class="btn">
-   </form>
+    <section class="form-container">
 
-</section>
+        <form action="" method="post">
+            <h3>créer un compte !</h3>
+            <input type="tel" name="name" required maxlength="50" placeholder="entrez votre nom" class="box">
+            <input type="email" name="email" required maxlength="50" placeholder="entrez votre email" class="box">
+            <input type="number" name="number" required min="0" max="9999999999" maxlength="10"
+                placeholder="entrez votre numéro" class="box">
+            <input type="password" name="pass" required maxlength="20" placeholder="entrez votre mot de passe"
+                class="box">
+            <input type="password" name="c_pass" required maxlength="20" placeholder="confirmez votre mot de passe"
+                class="box">
+            <p>vous avez déjà un compte ? <a href="login.php">connectez-vous maintenant</a></p>
+            <input type="submit" value="s'inscrire maintenant" name="submit" class="btn">
+        </form>
 
-<!-- register section ends -->
-
-
-
+    </section>
 
 
-
+    <!-- register section ends -->
 
 
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
-<?php include 'components/footer.php'; ?>
 
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
 
-<?php include 'components/message.php'; ?>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+    <?php include 'components/footer.php'; ?>
+
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
+
+    <?php include 'components/message.php'; ?>
 
 </body>
+
 </html>
