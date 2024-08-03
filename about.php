3874371c92a53fb5ec38953d277779b83950069c
Marcott <kasangandjomarcott47@gmail.com>
2024-08-03 10:52:55 +0200

    A bout us

--- about.php
+++ about.php
@@ -12,189 +12,198 @@ if(isset($_COOKIE['user_id'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>About Us</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
 
-   <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-   <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php include 'components/user_header.php'; ?>
 
-<!-- about section starts  -->
+    <?php include 'components/user_header.php'; ?>
 
-<section class="about">
+    <!-- about section starts  -->
 
-   <div class="row">
-      <div class="image">
-         <img src="images/about-img.svg" alt="">
-      </div>
-      <div class="content">
-         <h3>why choose us?</h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem provident voluptatum distinctio laborum veritatis vitae suscipit praesentium fugiat unde?</p>
-         <a href="contact.html" class="inline-btn">contact us</a>
-      </div>
-   </div>
+    <section class="about">
 
-</section>
-
-<!-- about section ends -->
-
-<!-- steps section starts  -->
-
-<section class="steps">
-
-   <h1 class="heading">3 simple steps</h1>
-
-   <div class="box-container">
-
-      <div class="box">
-         <img src="images/step-1.png" alt="">
-         <h3>search property</h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
-      </div>
-
-      <div class="box">
-         <img src="images/step-2.png" alt="">
-         <h3>contact agents</h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
-      </div>
-
-      <div class="box">
-         <img src="images/step-3.png" alt="">
-         <h3>enjoy property</h3>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
-      </div>
-
-   </div>
-
-</section>
-
-<!-- steps section ends -->
-
-<!-- review section starts  -->
-
-<section class="reviews">
-
-   <h1 class="heading">client's reviews</h1>
-
-   <div class="box-container">
-
-      <div class="box">
-         <div class="user">
-            <img src="images/pic-1.png" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
+        <div class="row">
+            <div class="image">
+                <img src="images/about-img.svg" alt="">
             </div>
-         </div>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio quam sequi error eum suscipit tempore inventore ex!</p>
-      </div>
-
-      <div class="box">
-         <div class="user">
-            <img src="images/pic-2.png" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
+            <div class="content">
+                <h3>Pourquoi nous choisir ?</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem provident voluptatum
+                    distinctio laborum veritatis vitae suscipit praesentium fugiat unde?</p>
+                <a href="contact.html" class="inline-btn">Nous contacter</a>
             </div>
-         </div>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio quam sequi error eum suscipit tempore inventore ex!</p>
-      </div>
+        </div>
 
-      <div class="box">
-         <div class="user">
-            <img src="images/pic-3.png" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
+    </section>
+
+    <!-- about section ends -->
+
+    <!-- steps section starts  -->
+
+    <section class="steps">
+
+        <h1 class="heading">3 étapes simples</h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="images/step-1.png" alt="">
+                <h3>recherche une propriété</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
             </div>
-         </div>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio quam sequi error eum suscipit tempore inventore ex!</p>
-      </div>
 
-      <div class="box">
-         <div class="user">
-            <img src="images/pic-4.png" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
+            <div class="box">
+                <img src="images/step-2.png" alt="">
+                <h3>contacter les agents</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
             </div>
-         </div>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio quam sequi error eum suscipit tempore inventore ex!</p>
-      </div>
 
-      <div class="box">
-         <div class="user">
-            <img src="images/pic-5.png" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
+            <div class="box">
+                <img src="images/step-3.png" alt="">
+                <h3>profiter de la propriété</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, placeat.</p>
             </div>
-         </div>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio quam sequi error eum suscipit tempore inventore ex!</p>
-      </div>
 
-      <div class="box">
-         <div class="user">
-            <img src="images/pic-6.png" alt="">
-            <div>
-               <h3>john deo</h3>
-               <div class="stars">
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star"></i>
-                  <i class="fas fa-star-half-alt"></i>
-               </div>
+        </div>
+
+    </section>
+
+    <!-- steps section ends -->
+
+    <!-- review section starts  -->
+
+    <section class="reviews">
+
+        <h1 class="heading">Avis des clients</h1>
+
+        <div class="box-container">
+
+            <div class="box">
+                <div class="user">
+                    <img src="images/pic-1.png" alt="">
+                    <div>
+                        <h3>john deo</h3>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio
+                    quam sequi error eum suscipit tempore inventore ex!</p>
             </div>
-         </div>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio quam sequi error eum suscipit tempore inventore ex!</p>
-      </div>
 
-   </div>
+            <div class="box">
+                <div class="user">
+                    <img src="images/pic-2.png" alt="">
+                    <div>
+                        <h3>john deo</h3>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio
+                    quam sequi error eum suscipit tempore inventore ex!</p>
+            </div>
 
-</section>
+            <div class="box">
+                <div class="user">
+                    <img src="images/pic-3.png" alt="">
+                    <div>
+                        <h3>john deo</h3>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio
+                    quam sequi error eum suscipit tempore inventore ex!</p>
+            </div>
 
-<!-- review section ends -->
+            <div class="box">
+                <div class="user">
+                    <img src="images/pic-4.png" alt="">
+                    <div>
+                        <h3>john deo</h3>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio
+                    quam sequi error eum suscipit tempore inventore ex!</p>
+            </div>
+
+            <div class="box">
+                <div class="user">
+                    <img src="images/pic-5.png" alt="">
+                    <div>
+                        <h3>john deo</h3>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio
+                    quam sequi error eum suscipit tempore inventore ex!</p>
+            </div>
+
+            <div class="box">
+                <div class="user">
+                    <img src="images/pic-6.png" alt="">
+                    <div>
+                        <h3>john deo</h3>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci voluptates delectus distinctio
+                    quam sequi error eum suscipit tempore inventore ex!</p>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- review section ends -->
 
 
 
@@ -205,10 +214,11 @@ if(isset($_COOKIE['user_id'])){
 
 
 
-<?php include 'components/footer.php'; ?>
+    <?php include 'components/footer.php'; ?>
 
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
+    <!-- custom js file link  -->
+    <script src="js/script.js"></script>
 
 </body>
+
 </html>
